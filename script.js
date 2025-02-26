0034b0a27a3a1bb6fa4683a73e72539d6104db5f
mluc404 <199426019+mluc404@users.noreply.github.com>
2025-02-25 16:21:00 -0800

    Code works. Next step: clean

--- script.js
+++ script.js
@@ -5,46 +5,43 @@
 // getNumSides()
 // createGrid()
 
-// Get user input
+// When the askButton is clicked, it'll prompt the user
+// to input a number for the size of the canvas,
+// then trigger the function to create the canvas.
+
+// Get user input and create canvas
 let askButton = document.querySelector("#askButton");
-let numSide = 0;
-
-askButton.addEventListener("click", () => {
-  numSide = parseInt(prompt("How many squares per side?"));
-  console.log(numSide);
-  createGrid();
-});
-
-// Set up canvas
 let container = document.querySelector("#container");
 let containerWidth = parseInt(window.getComputedStyle(container).width);
 
-function createGrid(numSide) {
-  alert("Here");
-}
+askButton.addEventListener("click", () => {
+  // Clean canvas first
+  container.innerHTML = "";
 
-let NUM = 16;
-// container.style.width = "400px";
-// container.style.height = "400px";
-console.log(container.style.width);
-console.log(parseInt(window.getComputedStyle(container).width));
-
-for (let i = 0; i < NUM; i++) {
-  for (let k = 0; k < NUM; k++) {
-    let square = document.createElement("div");
-    square.setAttribute("class", "square");
-    square.style.width = "25px";
-    square.style.height = "25px";
-    container.appendChild(square);
-  }
-}
-
-let allSquares = document.querySelectorAll(".square");
-allSquares.forEach((square) => {
-  square.addEventListener("mouseover", (e) => {
-    e.target.style.backgroundColor = "#575757";
-  });
-  //   square.addEventListener("mouseleave", (e) => {
-  //     e.target.style.backgroundColor = "";
-  //   });
+  // Get user input and create canvas
+  let input = parseInt(prompt("How many squares per side?"));
+  let numSide = parseInt(input);
+  createGrid(numSide);
 });
+
+// Function to Create canvas
+function createGrid(numSide) {
+  let squareSize = containerWidth / numSide;
+
+  for (let i = 0; i < numSide; i++) {
+    for (let k = 0; k < numSide; k++) {
+      let square = document.createElement("div");
+      square.setAttribute("class", "square");
+      square.style.width = squareSize.toString() + "px";
+      square.style.height = squareSize.toString() + "px";
+      container.appendChild(square);
+    }
+  }
+  // color on squares when mouse over
+  let allSquares = document.querySelectorAll(".square");
+  allSquares.forEach((square) => {
+    square.addEventListener("mouseover", (e) => {
+      e.target.style.backgroundColor = "#575757";
+    });
+  });
+}
